45edb59b84c0047c7199e6c28ecba2b6fa693a09
deni <i@ivanovdenis.com>
2022-09-02 08:53:39 +0300

    Module 3 done. And module 2 task 1 edited.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -2,9 +2,13 @@ function sumSalary(salaries) {
   let sum = 0;
 
   for (let key in salaries) {
-    typeof salaries[key] === "number" && !isNaN(salaries[key]) && salaries[key] !== Infinity && salaries[key] !== -Infinity
-      ? sum = sum + salaries[key]
-      : sum;
+    if (typeof salaries[key] === "number" &&
+      !isNaN(salaries[key]) &&
+      salaries[key] !== Infinity &&
+      salaries[key] !== -Infinity) {
+
+      sum = sum + salaries[key];
+    }
   }
   return sum;
 }
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split([]);
+  let changedArr = arr.filter((item) => {
+    if(item === '-') {
+      arr[arr.indexOf(item) + 1] = arr[arr.indexOf(item) + 1].toUpperCase();
+      delete arr[arr.indexOf(item)];
+    }else{
+      return item;
+    }
+  })
+  return changedArr.join('');
 }
+
+camelize('background-color-true');
