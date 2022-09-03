4a74d02f69a8ebe1ed8d3f93cd3ab97807e85d56
deni <i@ivanovdenis.com>
2022-09-03 12:53:23 +0300

    Module 4 done

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,16 @@
 function highlight(table) {
-  // ваш код...
+
+  for (const row of table.rows) {
+    let available = row.cells[3].dataset.available;
+    let gender = row.cells[2].textContent;
+    let age = row.cells[1].textContent;
+
+    row.classList.add(available === 'true' ? 'available' : 'unavailable');
+    row.hidden = available === undefined;
+    row.classList.add(gender === 'm' ? 'male' : 'female');
+
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
